94b09473f88c0090144238f143a9f299473eb73a
gaga <gagasuyu@gmail.com>
2025-03-02 15:20:04 +0100

    PORFI

--- dist/src/db-conection.js
+++ dist/src/db-conection.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.query = void 0;
+exports.query = query;
 var pg_1 = require("pg");
 var connectionString = 'postgresql://cartas_game_user:DqON4evQxb7BFUTJ3fB564YGcLbCBDwg@dpg-cum67qpu0jms73bkfjhg-a.frankfurt-postgres.render.com/cartas_game';
 var pool = new pg_1.Pool({
@@ -9,4 +9,3 @@ var pool = new pg_1.Pool({
 function query(text, params) {
     return pool.query(text, params);
 }
-exports.query = query;
