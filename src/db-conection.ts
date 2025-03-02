d16f7001dec1b75a987692e16611664c222ec439
gaga <gagasuyu@gmail.com>
2025-03-02 15:05:24 +0100

    asdasd

--- src/db-conection.ts
+++ src/db-conection.ts
@@ -1,14 +1,11 @@
 import { Pool } from 'pg';
 
-
-
-const connectionString = 'postgresql://cartas_game_user:DqON4evQxb7BFUTJ3fB564YGcLbCBDwg@dpg-cum67qpu0jms73bkfjhg-a.frankfurt-postgres.render.com/cartas_game'
-
+const connectionString = 'postgresql://cartas_game_user:DqON4evQxb7BFUTJ3fB564YGcLbCBDwg@dpg-cum67qpu0jms73bkfjhg-a.frankfurt-postgres.render.com/cartas_game';
 
 const pool = new Pool({
-  connectionString
-})
+  connectionString,
+});
 
-export function query(text: any): any {
-    return pool.query(text);
-}; 
+export function query(text: string, params?: any[]) {
+  return pool.query(text, params);
+}
