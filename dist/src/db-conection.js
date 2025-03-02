d27a86937df10d1aa0bb56bdce59f27510ab27be
gaga <gagasuyu@gmail.com>
2025-03-02 16:35:59 +0100

    asdasd

--- dist/src/db-conection.js
+++ dist/src/db-conection.js
@@ -1,12 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.query = void 0;
+exports.query = query;
 var pg_1 = require("pg");
+// Cadena de conexión proporcionada
 var connectionString = 'postgresql://cartas_game_user:DqON4evQxb7BFUTJ3fB564YGcLbCBDwg@dpg-cum67qpu0jms73bkfjhg-a.frankfurt-postgres.render.com/cartas_game';
 var pool = new pg_1.Pool({
     connectionString: connectionString,
+    ssl: {
+        rejectUnauthorized: false, // Esto desactiva la verificación estricta del certificado SSL.
+    },
 });
 function query(text, params) {
     return pool.query(text, params);
 }
-exports.query = query;
--- src/db-conection.ts
+++ src/db-conection.ts
@@ -1,11 +1,15 @@
 import { Pool } from 'pg';
 
+// Cadena de conexión proporcionada
 const connectionString = 'postgresql://cartas_game_user:DqON4evQxb7BFUTJ3fB564YGcLbCBDwg@dpg-cum67qpu0jms73bkfjhg-a.frankfurt-postgres.render.com/cartas_game';
 
 const pool = new Pool({
   connectionString,
+  ssl: {
+    rejectUnauthorized: false, // Esto desactiva la verificación estricta del certificado SSL.
+  },
 });
-   
+
 export function query(text: string, params?: any[]) {
   return pool.query(text, params);
 }
