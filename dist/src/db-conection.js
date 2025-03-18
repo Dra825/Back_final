cb29aa1771c71a0ea1d21b3645d0e63fc87bea32
gaga <gagasuyu@gmail.com>
2025-03-18 18:46:29 +0100

    asdasdGWEGTJKWIEGHJUW)EGEHUJIPhn

--- dist/src/db-conection.js
+++ dist/src/db-conection.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.query = query;
 var pg_1 = require("pg");
 // Cadena de conexión proporcionada
-var connectionString = 'postgresql://cartas_game_user:DqON4evQxb7BFUTJ3fB564YGcLbCBDwg@dpg-cum67qpu0jms73bkfjhg-a.frankfurt-postgres.render.com/cartas_game';
+var connectionString = 'postgresql://basededatos_gzip_user:nLlDgiA4riZ1am4IQSplRfKw1kH0FlrZ@dpg-cvcqlalumphs73afimog-a.frankfurt-postgres.render.com/basededatos_gzip';
 var pool = new pg_1.Pool({
     connectionString: connectionString,
     ssl: {
